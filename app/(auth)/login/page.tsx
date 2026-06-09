76423e5e973bc77385f44e91600639d59a8867de
Takumi Mito <250158hj@yse-c.net>
2026-06-09 15:29:17 +0900

    feat: プロジェクトルートからログイン画面へ静的リダイレクトの実装

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <div className="bg-sky-600">ここはログイン画面です</div>;
+}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1 className="bg-sky-600">Test</h1>;
-}
