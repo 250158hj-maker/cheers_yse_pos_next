effafa324c45c9a74aa3961fdf17d53c7b69f332
Takumi Mito <250158hj@yse-c.net>
2026-06-09 12:53:38 +0900

    fix: フォーム入力バリデーションの正規表現の修正

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,12 +1,10 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-    loginId: z
-        .string()
-        .regex(/^d{4}$/, "ログインIDは数字4桁で入力してください"),
-    password: z
-        .string()
-        .regex(/^d{4}$/, "パスワードは数字4桁で入力してください"),
+  loginId: z.string().regex(/^\d{4}$/, "ログインIDは数字4桁で入力してください"),
+  password: z
+    .string()
+    .regex(/^\d{4}$/, "パスワードは数字4桁で入力してください"),
 });
 
 export type LoginInput = z.infer<typeof loginSchema>;
