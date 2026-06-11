793020db046379b4120e8be1dea8567b2c66041c
Takumi Mito <250158hj@yse-c.net>
2026-06-11 16:25:39 +0900

    feat: ログインフォームの実装

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -6,6 +6,8 @@
 
 import { login } from "@/actions/auth";
 import { useActionState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function LoginForm() {
   const [state, formAction, isPending] = useActionState(login, null);
@@ -13,8 +15,9 @@ export default function LoginForm() {
   return (
     <div>
       <form action="post" method="post">
-        <input type="text" name="loginId" />
-        <input type="password" name="password" />
+        <Input type="text" name="loginId" />
+        <Input type="password" name="password" />
+        <Button />
       </form>
     </div>
   );
