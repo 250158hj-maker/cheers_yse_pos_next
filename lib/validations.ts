a14c4cc478be49a31edcb510b61d1f4de0ccc250
takumi <250158hj@yse-c.net>
2026-06-14 20:24:21 +0900

    fix: コンポーネントインストールとバリデーションエラーメッセージの修正

--- lib/validations.ts
+++ lib/validations.ts
@@ -6,10 +6,8 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-  loginId: z.string().regex(/^\d{4}$/, "ログインIDは数字4桁で入力してください"),
-  password: z
-    .string()
-    .regex(/^\d{4}$/, "パスワードは数字4桁で入力してください"),
+  loginId: z.string().regex(/^\d{4}$/, "ログインIDが正しくありません。"),
+  password: z.string().regex(/^\d{4}$/, "パスワードが正しくありません。"),
 });
 
 export type LoginInput = z.infer<typeof loginSchema>;
