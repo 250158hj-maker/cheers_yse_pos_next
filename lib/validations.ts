2979152f673e39006a10e052758e04594bf88bd9
takumi <250158hj@yse-c.net>
2026-06-17 00:33:46 +0900

    feat: カテゴリ作成バリデーションの実装

--- lib/validations.ts
+++ lib/validations.ts
@@ -5,9 +5,20 @@
 
 import z from "zod";
 
+// ログインフォーム入力
 export const loginSchema = z.object({
   loginId: z.string().regex(/^\d{4}$/, "ログインIDが正しくありません。"),
   password: z.string().regex(/^\d{4}$/, "パスワードが正しくありません。"),
 });
 
 export type LoginInput = z.infer<typeof loginSchema>;
+
+export const createCategorySchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, "カテゴリ名は必須です。")
+    .max(50, "カテゴリ名が長すぎます。"),
+});
+
+export type createCategoryInput = z.infer<typeof createCategorySchema>;
