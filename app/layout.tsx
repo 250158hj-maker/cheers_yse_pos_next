a6b1e8995fa62ac6088b6b02c5067302d361990d
Takumi Mito <250158hj@yse-c.net>
2026-06-23 10:17:19 +0900

    fix: 不要なコメントの削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
 /**
  * 全体レイアウト・Webページ本体
- * Reactではこの中身のDOMを差分検知して更新している（SPA）
  */
 
 import "./globals.css";
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="ja">
+            <body>{children}</body>
+        </html>
+    );
 }
